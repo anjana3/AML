be12378198b3f77bd740c385ab14b00be2ad1ccf
Anjana Narayana.K <devi.anjana5@gmail.com>
2020-01-02 17:46:32 +0530

    Updated code for bar graph

--- Data_Visualization.R
+++ Data_Visualization.R
@@ -7,14 +7,14 @@ library(lubridate)
 library(shinycustomloader)
 library(stringr)
 library(openxlsx)
+library(ggplot2)
 library(shinydashboard)
 library(shinydashboardPlus)
 
 Data_VisualizationUI <- function(id) {
   ns <- NS(id)
   
-  fluidPage(
-    fluidRow(
+  fluidPage(fluidRow(
     box(
       width = 12,
       class = "dataloading_class",
@@ -53,34 +53,58 @@ Data_VisualizationUI <- function(id) {
     )
   ),
   sidebarLayout(
-    sidebarPanel(width = 3,
-      box(width=15,title = "Ggplot2 and Highcharts",solidHeader = T,status = 'primary',
-          
-      radioButtons(ns("typeplot_radio"),label = "Please Select Type:",inline=T,choices = c("ggplot2","highchart"),selected = character(0))),
-
+    sidebarPanel(
+      width = 3,
+      box(
+        width = 15,
+        title = "Ggplot2 and Highcharts",
+        solidHeader = T,
+        status = 'primary',
+        
+        radioButtons(
+          ns("typeplot_radio"),
+          label = "Please Select Type:",
+          inline = T,
+          choices = c("ggplot2", "highchart"),
+          selected = character(0)
+        )
+      ),
+      
       uiOutput(ns("typeplot_text")),
       tags$hr(),
       uiOutput(ns("drop_xvariable")),
-      uiOutput(ns("drop_yvariable"))
+      uiOutput(ns("drop_yvariable")),
+      tags$hr(),
+      actionButton(ns("executegraph"), label = "Run Plot", class = "executegraph_class")
     ),
-    mainPanel(
-      fluidPage(
-      navbarPage("Choose graph",
-                 
-                 navbarMenu(tags$b("LINE CHARTS"), 
-                            tabPanel(tags$b("Basic Line"), 
-                                     plotOutput(ns("Basicline_plot_output"))
-                            ), 
-                            tabPanel(tags$b("Time Series"), plotOutput(ns("Timeseries_plot_output")))
-                 ),
-                 
-                 navbarMenu(tags$b("BARCHARTS"), 
-                            tabPanel(tags$b("Basic Bar"), plotOutput(ns("basicbar_plot_ouput"))), 
-                            tabPanel(tags$b("Stacked Bar"), plotOutput(ns("stackedbar_plot_output")))),
-                 id = "navbar"
-      )
-    )
-  )
+    mainPanel(fluidPage(
+      navbarPage(
+        "Choose graph",
+        
+        navbarMenu(
+          tags$b("LINE CHARTS"),
+          tabPanel(tags$b("Basic Line"),
+                   plotOutput(ns(
+                     "Basicline_plot_output"
+                   ))),
+          tabPanel(tags$b("Time Series"), plotOutput(ns(
+            "Timeseries_plot_output"
+          )))
+        ),
+        
+        navbarMenu(
+          tags$b("BARCHARTS"),
+          tabPanel(tags$b("Basic Bar"), plotOutput(ns(
+            "basicbar_plot_output"
+          ))),
+          tabPanel(tags$b("Stacked Bar"), plotOutput(ns(
+            "stackedbar_plot_output"
+          )))
+        ),
+        id = "navbar"
+      ),
+      downloadButton(ns("download_graph"), label = "Download Graph", class = "downloadgraph_class")
+    ))
   ))
 }
 
@@ -90,8 +114,9 @@ Data_Visualizationserver <- function(input, output, session) {
   
   values <- reactiveValues()
   plot   <- reactiveValues()
-  observe(
-  print(is.null(input$Basicline_plot_output)))
+  observe(print(is.null(input$Basicline_plot_output)))
+  
+  
   
   uploaded_Data <- reactive({
     file_to_read <- input$uploaded_file
@@ -104,7 +129,18 @@ Data_Visualizationserver <- function(input, output, session) {
     data_frame
   })
   
-  
+  observe({
+    uploaded <- uploaded_Data()
+    
+    if (is.null(uploaded)) {
+      disable(id = "executegraph")
+      disable(id = "View_dataset_visual")
+      
+    } else{
+      enable(id = "View_dataset_visual")
+      enable(id = "executegraph")
+    }
+  })
   
   observeEvent(input$View_dataset_visual, {
     showModal(
@@ -115,17 +151,17 @@ Data_Visualizationserver <- function(input, output, session) {
         style = "height:500px; overflow-y: scroll;overflow-x: scroll;",
         
         output$view_output <- renderDataTable({
-          datatable(uploaded_Data(),
-                    escape = FALSE,
-                    class = "cell-border",
-                    selection = 'single',
-                    options = list(
-                                   
-                                   aLengthMenu = list(c(5, 10, 20, 100,-1), list('5', '10', '20', '100', 'All')),
-                                   iDisplayLength = 5
-                                   
-                    )
-          ) 
+          datatable(
+            uploaded_Data(),
+            escape = FALSE,
+            class = "cell-border",
+            selection = 'single',
+            options = list(
+              aLengthMenu = list(c(5, 10, 20, 100,-1), list('5', '10', '20', '100', 'All')),
+              iDisplayLength = 5
+              
+            )
+          )
           
         })
       )
@@ -137,7 +173,6 @@ Data_Visualizationserver <- function(input, output, session) {
     uploaded <- uploaded_Data()
     if (is.null(uploaded)) {
       details_dataset <- paste("Please choose a dataset")
-      
     } else{
       rows <- nrow(uploaded)
       columns <- ncol(uploaded)
@@ -165,70 +200,125 @@ Data_Visualizationserver <- function(input, output, session) {
   
   # Render ouput for the radio button selected text
   
-  output$typeplot_text<-renderUI({
+  output$typeplot_text <- renderUI({
     values$radio_type_plot <- input$typeplot_radio
-    if(is.null(values$radio_type_plot)){
+    if (is.null(values$radio_type_plot)) {
+      
     }
     
-      else if (values$radio_type_plot == "highchart" ) 
-      {
-      statement <-paste0(values$radio_type_plot,"package enables the creation of Highcharts type plots within R.")      } 
-      else  
-          statement<-paste0(values$radio_type_plot," is based on the grammar of graphics, the
-      idea that you can build every graph from the same
-                            few components: a data set, a set of geoms-visual
-                            marks that represent data points, and a coordinate
-                            system.")
-        
+    else if (values$radio_type_plot == "highchart")
+    {
+      statement <-
+        paste0(
+          values$radio_type_plot,
+          "package enables the creation of Highcharts type plots within R."
+        )
+    }
+    else
+      statement <-
+        paste0(
+          values$radio_type_plot,
+          " is based on the grammar of graphics, the
+          idea that you can build every graph from the same
+          few components: a data set, a set of geoms-visual
+          marks that represent data points, and a coordinate
+          system."
+        )
+    
   })  # End of the render function
   
-  output$graph_type<-renderUI({
-    selectInput(ns("select_type_graph"),label = "Select type of Graph",choices = c("","",""))
+  output$graph_type <- renderUI({
+    selectInput(ns("select_type_graph"),
+                label = "Select type of Graph",
+                choices = c("", "", ""))
   })
   # render output for the xvarible in sidebar for the plot
   
-  output$drop_xvariable <-renderUI({
-    
-    if(is.null(uploaded_Data())){
-      selectInput(ns("dataset_xcolumn"),label = "Select Variable(X):",choices = c("Choose a Dataset"))
-    }else{
-        selectInput(ns("dataset_xcolumn"),label = "Select Variable(X):",choices = names(uploaded_Data()))
-  
-         }
+  output$drop_xvariable <- renderUI({
+    if (is.null(uploaded_Data())) {
+      selectInput(
+        ns("dataset_xcolumn"),
+        label = "Select Variable(X):",
+        choices = c("Choose a Dataset")
+      )
+    } else{
+      selectInput(
+        ns("dataset_xcolumn"),
+        label = "Select Variable(X):",
+        choices = names(uploaded_Data())
+      )
+      
+    }
   })  # End of the render output
   
   # render output for the y varible in sidebar for the plot
   
-  output$drop_yvariable <-renderUI({
-    
-    if(is.null(uploaded_Data())){
-      selectInput(ns("dataset_ycolumn"),label = "Select Variable(Y):",choices = c("Choose a Dataset"))
-    }else{
-      selectInput(ns("dataset_ycolumn"),label = "Select Variable(Y):",choices = names(uploaded_Data()))
+  output$drop_yvariable <- renderUI({
+    if (is.null(uploaded_Data())) {
+      selectInput(
+        ns("dataset_ycolumn"),
+        label = "Select Variable(Y):",
+        choices = c("Choose a Dataset")
+      )
+    } else{
+      selectInput(
+        ns("dataset_ycolumn"),
+        label = "Select Variable(Y):",
+        choices = names(uploaded_Data())
+      )
     }
   })
-
   
-Dataset_graph <- function() {
-       if(is.null(uploaded_Data())){
-    Example_df<-mtcars
-    values$dataset_inputgraph <- data.frame(x = Example_df$hp,y = Example_df$carb)
-    
-      }else{
-        values$dataset_inputgraph <-data.frame(uploaded_Data() %>% select(input$dataset_ycolumn),uploaded_Data() %>% select(input$dataset_xcolumn))
-    
+  
+  Dataset_graph <- function() {
+    if (is.null(uploaded_Data())) {
+      Example_df <- mtcars
+      values$dataset_inputgraph <-
+        data.frame(x = Example_df$hp, y = Example_df$carb)
+      
+    } else{
+      values$dataset_inputgraph <-
+        data.frame(
+          uploaded_Data() %>% select(input$dataset_xcolumn),
+          uploaded_Data() %>% select(input$dataset_ycolumn)
+        )
+      
     }
-}    
- 
-  output$Basicline_plot_output<-renderPlot({
-        Dataset_graph()
-          ggplot(data = values$dataset_inputgraph, aes(x = values$dataset_inputgraph[,1], y = values$dataset_inputgraph[,2])) +
-            geom_line()
+  }
+  
+  output$Basicline_plot_output <- renderPlot({
+    Dataset_graph()
+    ggplot(
+      data = values$dataset_inputgraph,
+      aes(
+        x = values$dataset_inputgraph[, 1],
+        y = values$dataset_inputgraph[, 2]
+      )
+    ) +
+      geom_line() + ggtitle(label = "Example Graph for Basic Line") +
+      geom_point()
   })
   
-
- output$Timeseries_plot_output<-renderPlot({
-   
- })
+  output$basicbar_plot_output <- renderPlot({
+    Dataset_graph()
+    
+    ggplot(
+      data = values$dataset_inputgraph,
+      aes(
+        x = values$dataset_inputgraph[, 1],
+        #y = values$dataset_inputgraph[, 2],
+      fill = values$dataset_inputgraph[, 2]
+        )
+    ) + 
+      geom_bar() +
+      geom_text(aes(label = ..count..),
+                stat = "count",
+                position = position_stack(0.5)) + coord_flip() +
+      theme(plot.background = element_rect(fill = "#ECF0F5"))
+    
+  })
+  output$Timeseries_plot_output <- renderPlot({
+    
+  })
   
 }  # End of the server
