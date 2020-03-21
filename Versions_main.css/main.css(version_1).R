0c2c68f52167675e379e4685b493a19d973b3aea
Anjana Narayana.K <devi.anjana5@gmail.com>
2020-03-21 17:45:25 +0530

    Main.css updted code for data loading into sidebar

---
+++ Versions_main.css/main.css(version_1).R
@@ -0,0 +1,176 @@
+.skin-blue .left-side, .skin-blue .main-sidebar, .skin-blue .wrapper {
+    background-color: rgb(57, 51, 83);
+}
+.well {
+    min-height: 20px;
+    padding: 19px;
+    margin-bottom: 20px;
+    background-color: #a78e8eb3;
+    border: 2px solid rgb(122, 165, 217);
+    border-radius: 4px;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.05);
+    box-shadow: inset 0 1px 1px rgba(147, 40, 40, 0.05);
+}
+.box-header {
+    color: #fff;
+    display: block;
+    padding: 10px;
+    position: relative;
+    background-color:navy;
+}
+
+.dataloading_class {
+    position: relative;
+}
+.box-body {
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    background: #ffffff;
+    border-bottom-right-radius: 3px;
+    border-bottom-left-radius: 3px;
+    padding: 6px;
+}
+
+
+.direct-chat-contacts {
+    -webkit-transform: translate(100%, 0);
+    -ms-transform: translate(100%, 0);
+    -o-transform: translate(100%, 0);
+    transform: translate(100%, 0);
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    background: #e9557e;
+    color: #ffff;
+    overflow: auto;
+}
+.ggplot_class{
+  padding-right:0px;
+  padding-left:0px;
+}
+.highchart_class{
+  padding-right:0px;
+  padding-left:0px;
+}
+.content-wrapper, .right-side {
+    min-height: 100%;
+    background-color: #f0f3f6;
+    z-index: 800;
+}
+.info-box {
+    background: #82ce63;
+    width: 388%;
+    color: #ffff;
+     margin-left: -13px; 
+}
+
+.view_button_class {
+    border-color: #27774f;
+    height: 44px;
+    background: #605ca8;
+    color: rgb(255, 255, 255);
+    border-radius: 6px;
+    /* margin-bottom: 10px; */
+    
+}
+.box_ggplot_class {
+    position: relative;
+    background: #ffffff;
+    }
+.btn-box-tool {
+    padding: 5px;
+    font-size: 12px;
+    background: 0 0;
+    color: rgb(255, 255, 255);
+}
+
+.box_ggplot_class.box-solid{
+      border: 1px solid green;
+
+}
+.modal-title {
+    margin: 10;
+    /* line-height: 0.428571; */
+    /* background-color: beige; */
+    color: darkslateblue;
+    font-weight: 900;
+    font-size: 20px;
+    padding-bottom: -8px;
+    /* margin: 4px; */
+}
+
+.well {
+    /* min-height: 20px; */
+    padding: 9px;
+    /* margin-bottom: 21px; */
+    background-color: rgb(255, 255, 255);
+    border: 2px solid rgb(122, 165, 217);
+    border-radius: 4px;
+    /* -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.05); */
+    /* box-shadow: inset 0 1px 1px rgba(147, 40, 40, 0.05); */
+}
+.dropdown-toggle b, .navbar-brand {
+    font-weight: 800;
+    color: rgb(13, 39, 52);
+}
+
+
+.navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover {
+    color: #555;
+    background-color: rgb(255, 255, 255);
+    width: 148px;
+    padding-left: 14px;
+
+}
+.navbar-default {
+    background-color: rgb(255, 255, 255);
+    border-color: rgba(93, 99, 94, 0.07);
+    font-family: -webkit-body;
+    /* color: brown; */
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+    background-color: #fff;
+    border-radius: 2px;
+        margin-left: -24px;
+
+    /* border-right: azure; */
+}
+.navbar-brand {
+    border-right: rgba(149, 160, 166, 0.23) 2px solid;
+}
+
+
+.nav-boxed {
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+    background-color: #fff;
+    border-radius: 2px;
+}
+
+.navbar-nav>li {
+    float: left;
+    border-right: rgba(149, 160, 166, 0.23) 2px solid;
+}
+
+.downloadgraph_class {
+    background-color: #605ca8;
+    color: rgb(255, 255, 255);
+    border-color: rgb(75, 156, 112);
+    margin-top: 15px;
+}
+
+.btn-default {
+    background-color: #605ca8;
+    color: rgb(255, 255, 255);
+    border-color: rgb(75, 156, 112);
+}
+
+.executegraph_class{
+   background-color: #605ca8;
+    color: rgb(255, 255, 255);
+    border-color: rgb(75, 156, 112);
+}
+
+.adddesign_class{
+  padding-top:18px;
+}
